0f2e54e95ad0e454537f5b7e7720aca1bb403016
Gustavo Santos <gustavolimarma@gmail.com>
2026-02-18 17:25:47 -0300

    Department insert implementation

---
+++ src/main/java/application/Program2.java
@@ -0,0 +1,21 @@
+package application;
+
+import db.DB;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+    public static void main(String[] args) {
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: Department insert ===");
+        Department department = new Department(null, "Mobile");
+        departmentDao.insert(department);
+        System.out.println("Inserted! New id = " + department.getId());
+
+        DB.closeConnection();
+    }
+}
---
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,68 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection connection;
+
+    public DepartmentDaoJDBC(Connection connection){
+        this.connection = connection;
+    }
+
+    @Override
+    public void insert(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = connection.prepareStatement(
+                   "INSERT INTO department "
+                    + "(Name) "
+                    + "VALUES "
+                    + "(?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1, obj.getName());
+
+            int rowAffected = st.executeUpdate();
+
+            if (rowAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    @Override
+    public void update(Department obj) {
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
